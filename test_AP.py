e9f1e103e49404c7f74f056f9bbcbdf4f8b2b2d5
Mirko <mucciarinimirko@gmail.com>
2019-06-16 23:23:53 +0200

    videos

--- test_AP.py
+++ test_AP.py
@@ -8,8 +8,7 @@ import argparse
 import numpy as np
 import pandas as pd
 from sklearn.metrics import average_precision_score
-
-# from yolov3.detection_2 import Yolo
+from yolov3.detection_2 import Yolo
 from tools.utils import non_max_suppression, read_labels
 from tools.metrics import match_pred_gt
 
